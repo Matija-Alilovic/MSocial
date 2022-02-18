d6d1663b8a171a72d42f64ab69b9de32917125da
Matematko123 <alilovicmatija2@gmail.com>
2022-02-18 11:42:02 +0100

    finished CRUD

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const helmet = require('helmet');
 const morgan = require('morgan');
 const userRoute = require('./routes/users.js');
 const authRoute = require('./routes/auth.js');
+const postRoute = require('./routes/posts.js');
 
 dotenv.config();
 
@@ -20,6 +21,7 @@ app.use(morgan('common'));
 
 app.use('/api/users', userRoute);
 app.use('/api/auth', authRoute);
+app.use('/api/posts', postRoute);
 
 app.listen(8800, () => {
   console.log('Backend server is running!');
